570a1306b4bde9acb083afdc2bdcd815538fc77d
Maria Koczorowska <mariakoczorowska@mania-macbook.local>
2026-03-18 14:02:49 +0100

    .

--- 04_03_2026/frontend/src/components/Footer/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './Footer.module.scss'
-
-export default function Footer() {
-    return (
-        <footer className={styles.Footer}>
-            Copyright &copy; {new Date().getFullYear()} | Stafecki
-        </footer>
-    )
-}
--- 04_03_2026/frontend/src/hooks/usePosts.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from '@tanstack/react-query';
-import { fetchPosts } from '../api/api';
-
-export const usePosts = (categoryId?: number) => {
-  return useQuery({
-    queryKey: ['posts', categoryId],
-    queryFn: () => fetchPosts(categoryId),
-  });
-};
